b7b0e193403c2a89124099def844e017dc0176e7
Arinze Anakor <arinzeanakor@gmail.com>
2020-05-01 19:10:42 +0100

    feature(chat app): basic chat app following Angela Yu Swift iOS course on Udemy

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var labelStatus: UILabel!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    self.labelStatus.text = e.localizedDescription
+                    return
+                }
+                
+                self.performSegue(withIdentifier: K.registerSegue, sender: self)
+            }
+        }
     }
-    
 }
--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,11 +12,27 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-       
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
 
+        for charachter in K.appName {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) {
+                (timer) in self.titleLabel.text?.append(charachter)
+            }
+            charIndex += 1
+        }
+    }
 }
